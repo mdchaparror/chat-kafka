70cb89efed2fe51bd9e8dc9812c7fc7b6d6a1c7e
martin chaparro <mdchaparror@gmail.com>
2020-10-25 15:57:00 -0500

    ultimos cambios

--- src/index.js
+++ src/index.js
@@ -1,77 +1,66 @@
-
-const express = require('express')
-const path = require('path')
-const { Kafka, logLevel } = require('kafkajs/index')
-const SocketIO = require('socket.io')
-
-
-const host = 'localhost'
-
+const express = require("express");
+const path = require("path");
+const { Kafka, logLevel } = require("kafkajs/index");
+const SocketIO = require("socket.io");
+const { emit } = require("process");
+const host = "localhost";
 const app = express();
-//Configuraciones
-app.set('port',3000);
-console.log(__dirname)
-app.use(express.static(path.join(__dirname,"public")))
-const server = app.listen(app.get('port'),()=>{
-  console.log('Servidor iniciado puerto: ',app.get('port'))
-})
 
-const io = SocketIO(server)
+//Configuraciones
+app.set("port", 3000);
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "public")));
+const server = app.listen(app.get("port"), () => {
+  console.log("Servidor iniciado puerto: ", app.get("port"));
+});
+
+const io = SocketIO(server);
 
 const kafka = new Kafka({
   logLevel: logLevel.NOTHING,
   brokers: [`${host}:9092`],
-  clientId: 'example-consumer',
-})
-
+  clientId: "example-consumer",
+});
 
 //const topic = 'myTopic'
-const consumer = kafka.consumer({ groupId: 'test-group' })
+const consumer = kafka.consumer({ groupId: "test-group" });
 
+const conection = async () => {
+  await consumer.connect();
+  console.log("Conectado a Kafka");
+};
+conection().catch((e) => {
+  console.error("Se presento un error conectando", e)
+});
 
-const conection = async () =>{
- 
-  await consumer.connect()
-  console.log('Conectado a Kafka')
-}
-conection().catch(e => console.error('Se presento un error conectando', e))
-
-
-
-
-const subscribe =  async (topic) =>{
-  await consumer.subscribe({ topic, fromBeginning: true })
-  console.log('Suscrito al topic: ',topic)
-}
-
-
-
-
+const subscribe = async (topic) => {
+  await consumer.subscribe({ topic, fromBeginning: true });
+  io.sockets.emit('OK',{'mensaje':'Suscrito al topic: '+topic})
+  console.log("Suscrito al topic: ", topic);
+};
 
 const run = async () => {
- 
- 
-  
- 
-  console.log('Esperando mensajes')
+  console.log("Esperando mensajes");
   await consumer.run({
-
     eachMessage: async ({ topic, partition, message }) => {
-   
-      console.log('Nuevo mensaje: ',message.value.toString())
-      io.sockets.emit('mensaje',message.value.toString())
+      console.log("Nuevo mensaje: ", message.value.toString());
+      io.sockets.emit("mensaje", message.value.toString());
     },
-  })
-}
+  });
+};
 
 
 //websockect
-io.on('connection',(socket)=>{
-  console.log('Cliente conectado')
-  socket.on('subscribe_topic',(topic)=>{
-    console.log(topic)
-    subscribe(topic).catch(e => console.error('Se presento un error suscribiendo', e))
-    run().catch(e => console.error('Se presento un error', e))
-  })
-})
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
+  socket.on("subscribe_topic", (topic) => {
+    console.log(topic);
+    subscribe(topic).catch((e) =>{
+      console.error("Se presento un error suscribiendo", e)
+      io.sockets.emit('Error',{'mensaje':'Servidor Kafka no iniciado','error':e})
+    }
 
+    );
+    run().catch((e) => console.error("Se presento un error", e));
+  });
+});
--- src/public/kafka.js
+++ src/public/kafka.js
@@ -5,6 +5,7 @@ let subscribe_btn = document.getElementById('subscribe_btn');
 let output = document.getElementById('output');
 let mensaje = document.getElementById('mensaje');
 let send = document.getElementById('send');
+let alertas = document.getElementById('alertas');
 
 subscribe_btn.addEventListener('click', function () {
     console.log({
@@ -19,4 +20,17 @@ socket.on('mensaje',(mensaje)=>{
     <h3><span class="badge badge-success">${mensaje}</span></h3>
     
 </div>`
+})
+
+
+socket.on('Error',(mensaje)=>{
+    console.log(mensaje.mensaje)
+    alertas.className = 'alert alert-danger alert-dismissible fade show'
+    alertas.innerHTML = mensaje.mensaje
+})
+
+socket.on('OK',(mensaje)=>{
+    console.log(mensaje.mensaje)
+    alertas.className = 'alert alert-success alert-dismissible fade show'
+    alertas.innerHTML = mensaje.mensaje
 })
